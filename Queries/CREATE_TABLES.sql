6b1725ffad5a074065d002d676c71328f572bfc5
vupha <vupham129@gmail.com>
2022-04-11 23:33:55 -0700

    HOMEWORK

--- Queries/CREATE_TABLES.sql
+++ Queries/CREATE_TABLES.sql
@@ -1,93 +1,21 @@
 /*
- CUSTOMER TABLE COLUMNS :
-    CustomerID
-    CustomerName
-    ContactName
-    Address
-    City
-    PostalCode
-    Country
+ -- 1. FIND OUT THE CUSTOMER COUNT FOR EACH COUNTRY
+ SELECT Country,count(*) FROM Customers
+ group by Country;
+-- 2. FIND OUT ANY EMPLOYEES WITH BA DEGREES (BY CHECKING NOTES CONTAINING BA )
 
-    RELATED TO ORDERS TABLE BY CustomerID COLUMN
+-- 3. FIND OUT CUSTOMER NAME AND OrderID  Using the Orders and Customers table
+-- 4. FIND OUT CUSTOMER NAME AND OrderID  Using the Orders and Customers table
+-- CONTINUE TO FIND OUT THE EMPLOYEE NAME  USING EmployeeID
+-- 5. Find out OrderDetailID and ProductName using the OrderDetails table and Products table
+-- they are related by ProductID
 
-ORDERS TABLE COLUMNS :
-    OrderID
-    CustomerID
-    EmployeeID
-    OrderDate
-    ShipperID
-
-    RELATED TO CUSTOMER TABLE BY CustomerID COLUMN
-    RELATED TO EMPLOYEES TABLE BY EmployeeID COLUMN
-    RELATED TO ORDERDETAILS TABLE BY OrderID COLUMN
-    RELATED TO SHIPPERS COLUMN BY ShipperID COLUMN
-
-EMPLOYEES TABLE COLUMNS
-    EmployeeID
-    LastName
-    FirstName
-    BirthDate
-    Photo
-    Notes
-
-     RELATED TO ORDERS TABLE BY EmployeeID COLUMN
-
-ORDERDETAILS TABLE COLUMNS :
-    OrderDetailID
-    OrderID
-    ProductID
-    Quantity
-
-    RELATED TO ORDERS TABLE BY OrderID COLUMN
-    RELATED TO PRODUCTS TABLE BY ProductID COLUMN
-
-PRODUCTS TABLE COLUMNS
-
-    ProductID
-    ProductName
-    SupplierID
-    CategoryID
-    Unit
-    Price
-
-    RELATED TO SUPPLIER TABLE BY SupplierID COLUMN
-    RELATED TO CATEGORIES TABLE BY CategoryID COLUMN
-
-SHIPPERS TABLE COLUMNS
-    ShipperID
-    ShipperName
-    Phone
-
-    RELATED TO ORDERS TABLE BY ShipperID COLUMN
-
-CATEGORIES TABLE COLUMNS
-    CategoryID
-    CategoryName
-    Description
-
-    RELATED TO PRODUCTS TABLE BY  CategoryID COLUMN
- */
+OPTIONAL TASK
+-- O1. FIND OUT HOW MANY ORDERS EACH CUSTOMER MADE, INCLUDING THOSE CUSTOMERS WHO MADE NO ORDER
+--O2. -- FIND OUT COUNT OF PRODUCTS FOR EACH ORDER FROM OrderDetails table
+--O3.  FIND OUT EMPLOYEE FirstName and Count of ORDER they processed
+-- including those employees did not process any order (Adam)
+--O4. Find out How much each ORDER COST - SUM OF ALL PRODUCT PRICE IN ORDER DETAILS FOR EACH ORDER
 
 
---  CUSTOMER TABLE COLUMNS :
-
-CREATE TABLE CUSTOMER_NEW(
-    CUSTOMER_ID INTEGER PRIMARY KEY,
-    CUSTOMER_NAME VARCHAR(50) NOT NULL,
-    CONTACT_NAME VARCHAR(50),
-    ADDRESS VARCHAR(50), CITY VARCHAR(50), POSTAL_CODE INTEGER, COUNTRY VARCHAR(50)
-);
-
-/*
- ORDERS TABLE COLUMNS :
-    OrderID
-    CustomerID
-    EmployeeID
-    OrderDate
-    ShipperID
-
-    RELATED TO CUSTOMER TABLE BY CustomerID COLUMN
-    RELATED TO EMPLOYEES TABLE BY EmployeeID COLUMN
-    RELATED TO ORDERDETAILS TABLE BY OrderID COLUMN
-    RELATED TO SHIPPERS COLUMN BY ShipperID COLUMN
  */
